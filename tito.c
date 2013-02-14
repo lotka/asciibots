8a248accc45ec06404ef440ae84f7dd411bed406
Luka Milic <lukam321@gmail.com>
2013-02-14 15:14:06 +0000

    Update tito.c

--- tito.c
+++ tito.c
@@ -1,9 +1,11 @@
 /* Plan
 
+
+
 Priorities
-0. Don't get hit by a bullet
-1. If under 40% hp find three cells where your sure no mines were placed and dodge bullets from there
-2. Record possible mine positions, never go on these suspected coordinates
+0. Don't get hit by an incoming bullet
+1. Record possible mine positions, never go on these suspected coordinates
+2. If under 40% hp find three cells where your sure no mines were placed and dodge bullets from there
 2. Line up with other players from a distance and fire
 3. Lay mines if bored
 4. If alone shoot the walls like a maniac
